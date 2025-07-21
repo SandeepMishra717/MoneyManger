81aa3f9cab0721adcba998ff66287ebc8b23d31c
SandeepMishra717 <abcdsandeep4u@gmail.com>
2025-07-22 00:20:57 +0530

    intial structure of project

---
+++ moneymanager/src/test/java/in/sandeep/moneymanager/MoneymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.sandeep.moneymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
