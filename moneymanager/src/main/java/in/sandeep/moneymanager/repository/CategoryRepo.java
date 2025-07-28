4c3e3893bfef947ef4a6128384bda140a8cc935c
SandeepMishra717 <abcdsandeep4u@gmail.com>
2025-07-28 23:45:35 +0530

    Category and Expenses part

---
+++ moneymanager/src/main/java/in/sandeep/moneymanager/repository/CategoryRepo.java
@@ -0,0 +1,22 @@
+package in.sandeep.moneymanager.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.sandeep.moneymanager.entity.CategoryEntity;
+
+public interface CategoryRepo extends JpaRepository<CategoryEntity, Long> {
+
+    //select * from tbl_categories where profile_id = ?1
+    List<CategoryEntity> findByProfileId(Long profileId);
+
+    //select * from tbl_categories where id = ?1 and profile_id = ?2
+    Optional<CategoryEntity> findByIdAndProfileId(Long id, Long profileId);
+
+    //select * from tbl_categories where type = ?1 and profile_id = ?2
+    List<CategoryEntity> findByTypeAndProfileId(String type, Long profileId);
+
+    Boolean existsByNameAndProfileId(String name, Long profileId);
+}
