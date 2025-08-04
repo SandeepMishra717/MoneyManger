b36b2ee1ad7851bf88ac1e0cdb7a2cb49700597d
SandeepMishra717 <abcdsandeep4u@gmail.com>
2025-08-04 23:40:07 +0530

    Email, Excell part

---
+++ moneymanager/src/main/java/in/sandeep/moneymanager/controller/EmailController.java
@@ -0,0 +1,58 @@
+package in.sandeep.moneymanager.controller;
+
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.sandeep.moneymanager.entity.ProfileEntity;
+import in.sandeep.moneymanager.service.EmailService;
+import in.sandeep.moneymanager.service.ExpenseService;
+import in.sandeep.moneymanager.service.IncomeService;
+import in.sandeep.moneymanager.service.ProfileService;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/email")
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final ExcelService excelService;
+    private final IncomeService incomeService;
+    private final ExpenseService expenseService;
+    private final EmailService emailService;
+    private final ProfileService profileService;
+
+    @GetMapping("/income-excel")
+    public ResponseEntity<Void> emailIncomeExcel() throws IOException, MessagingException {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        excelService.writeIncomesToExcel(baos, incomeService.getCurrentMonthIncomesForCurrentUser());
+        emailService.sendEmailWithAttachment(profile.getEmail(),
+                "Your Income Excel Report",
+                "Please find attached your income report",
+                baos.toByteArray(),
+                "income.xlsx");
+        return ResponseEntity.ok(null);
+    }
+
+    @GetMapping("/expense-excel")
+    public ResponseEntity<Void> emailExpenseExcel() throws IOException, MessagingException {
+        ProfileEntity profile = profileService.getCurrentProfile();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        excelService.writeExpensesToExcel(baos, expenseService.getCurrentMonthExpensesForCurrentUser());
+        emailService.sendEmailWithAttachment(
+                profile.getEmail(),
+                "Your Expense Excel Report",
+                "Please find attached your expense report.",
+                baos.toByteArray(),
+                "expenses.xlsx");
+        return ResponseEntity.ok(null);
+    }
+}
